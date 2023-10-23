44e97d6e4ca0adb1e60d6cd716766b7b7dbd84e5
Goyal <goyalsiddharth315@gmail.com>
2023-10-23 15:49:13 +0530

    first commit

---
+++ app/src/main/java/com/example/erpsystem/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.erpsystem
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+
+    // splash screen
+    lateinit var handler : Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this,Login::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+
+    }
+}
